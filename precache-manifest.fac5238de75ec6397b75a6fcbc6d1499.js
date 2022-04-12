aff994cd3ab244f0e53e1a4378ad12d238b61e60
trongnghia0805 <103453259+Simonjay85@users.noreply.github.com>
2022-04-11 23:16:55 -0400

    Updates

---
+++ precache-manifest.fac5238de75ec6397b75a6fcbc6d1499.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "defc8d09f0ae032daf8dbb5e62aaad4a",
+    "url": "/porfolio/index.html"
+  },
+  {
+    "revision": "2118b8f277f50b86ad6d",
+    "url": "/porfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "728f25bc9f5228744f20",
+    "url": "/porfolio/static/css/main.db76a3b0.chunk.css"
+  },
+  {
+    "revision": "2118b8f277f50b86ad6d",
+    "url": "/porfolio/static/js/2.5f6037e9.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/porfolio/static/js/2.5f6037e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "728f25bc9f5228744f20",
+    "url": "/porfolio/static/js/main.4d5106fc.chunk.js"
+  },
+  {
+    "revision": "1f19dcc9a8de44bcebbf",
+    "url": "/porfolio/static/js/runtime-main.11c265cb.js"
+  }
+]);
